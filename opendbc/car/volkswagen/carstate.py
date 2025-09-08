7665b6c38546c3e48693f1c62849aa97b5c14f9e
DragoTheWolf <dragotehwolf@gmail.com>
2025-09-08 03:18:01 -0400

    updates

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -209,6 +209,7 @@ class CarState(CarStateBase):
     self.acc_type = 0
     ret.cruiseState.available = True
     ret.cruiseState.enabled = True
+    ret.accFaulted = False
 
     # Update ACC setpoint. When the setpoint reads as 255, the driver has not
     # yet established an ACC setpoint, so treat it as zero.
